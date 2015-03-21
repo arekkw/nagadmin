e2a8475ce589e1d1b89232337ae316bb1dce5f01
Robert Hubbard <bobbyhubbard@gmail.com>
2015-03-21 23:00:37 +0000

    something works

---
+++ app/components/nag-es-autocomplete.js
@@ -0,0 +1,22 @@
+import AutoComplete from "ember-cli-auto-complete/components/auto-complete";
+
+export default AutoComplete.extend({
+  valueProperty: "code",
+  suggestions: function() {
+      var inputVal = this.get("inputVal") || "";
+      return this.get("options").filter(function(item) {
+          //return item.get("code").toLowerCase().indexOf(inputVal.toLowerCase()) > -1;
+          return item.code.toLowerCase().indexOf(inputVal.toLowerCase()) > -1;
+      });
+  }.property("inputVal", "options.@each"),
+  optionsToMatch: function() {
+      var caseInsensitiveOptions = [];
+      this.get("options").forEach(function(item) {
+          //var value = item.get("code");
+          var value = item.code;
+          caseInsensitiveOptions.push(value);
+          caseInsensitiveOptions.push(value.toLowerCase());
+      });
+      return caseInsensitiveOptions;
+  }.property("options.@each")
+});
--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -2,5 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     breadCrumb: "Home",
-	breadCrumbIconClass: "ace-icon fa fa-home home-icon"
+	breadCrumbIconClass: "ace-icon fa fa-home home-icon",
+	codes:[],
+        
+    init: function(){
+        this._super();
+        var codes = this.get("codes");
+        codes.push({code: "ABC", text: "SOMETHING 1"});
+        codes.push({code: "ABCD", text: "SOMETHING 2"});
+        codes.push({code: "ABCDE", text: "SOMETHING 3"});
+        this.set("codes", codes);
+    }
 });
