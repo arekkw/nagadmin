0569ca7f6c940595829e9c213df2f53ea5b4eecd
Robert Hubbard <bobbyhubbard@gmail.com>
2015-03-22 17:15:53 +0000

    styled - still missing select functionality

--- app/components/nag-es-autocomplete.js
+++ app/components/nag-es-autocomplete.js
@@ -1,22 +1,57 @@
 import AutoComplete from "ember-cli-auto-complete/components/auto-complete";
+import config from '../config/environment';
 
 export default AutoComplete.extend({
-  valueProperty: "code",
-  suggestions: function() {
-      var inputVal = this.get("inputVal") || "";
-      return this.get("options").filter(function(item) {
-          //return item.get("code").toLowerCase().indexOf(inputVal.toLowerCase()) > -1;
-          return item.code.toLowerCase().indexOf(inputVal.toLowerCase()) > -1;
-      });
-  }.property("inputVal", "options.@each"),
+    valueProperty: "code",
+    options:[],
+    suggestions: function() {
+        return this.get("options");
+    }.property("options.@each"),
+    
+    loadMatches: function() {
+        var self = this;
+        var inputVal = this.get("inputVal");
+        if(!inputVal || inputVal.length < 2){
+            return;
+        }
+        var esSearchBody = {
+            "name-suggest" : {
+                "text" : inputVal,
+                "completion" : {
+                    "field" : "suggest",
+                    "fuzzy" : {
+                        "fuzziness" : 2
+                    }
+                }
+            }
+        };
+        Em.$.ajax({
+            type: "POST",
+            url: config.elasticsearchPath + '/orgs,contacts/_suggest',
+            data: JSON.stringify(esSearchBody),
+            success: function(data){
+                self.set('options', []);
+                var store = self.get('container').lookup('store:main');
+                Em.$.each(data["name-suggest"][0].options, function(i,obj){
+                    if(obj.payload.contactId){
+                        store.find('contact', obj.payload.contactId).then(function(contact){
+                            self.get('options').pushObject(contact);
+                        });
+                    } else if(obj.payload.orgId){
+                        store.find('organizations/org', obj.payload.orgId).then(function(org){
+                            self.get('options').pushObject(org.get('profile'));
+                        });
+                    }
+                });
+            },
+            error: function(e){
+                console.error("Unable to search." + e);
+            },
+            dataType: 'json'
+        });
+  }.observes("inputVal"),
+  
   optionsToMatch: function() {
-      var caseInsensitiveOptions = [];
-      this.get("options").forEach(function(item) {
-          //var value = item.get("code");
-          var value = item.code;
-          caseInsensitiveOptions.push(value);
-          caseInsensitiveOptions.push(value.toLowerCase());
-      });
-      return caseInsensitiveOptions;
+      return this.get("options");
   }.property("options.@each")
 });
--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -4,13 +4,9 @@ export default Ember.Controller.extend({
     breadCrumb: "Home",
 	breadCrumbIconClass: "ace-icon fa fa-home home-icon",
 	codes:[],
-        
-    init: function(){
-        this._super();
-        var codes = this.get("codes");
-        codes.push({code: "ABC", text: "SOMETHING 1"});
-        codes.push({code: "ABCD", text: "SOMETHING 2"});
-        codes.push({code: "ABCDE", text: "SOMETHING 3"});
-        this.set("codes", codes);
-    }
+	
+	_selectedValue: function(e,a){
+	    console.log(e);
+	    console.log(a);
+	}.observes('selectedValue')
 });
