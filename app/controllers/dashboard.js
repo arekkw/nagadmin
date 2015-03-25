bd11a4b98dbc50c187c5ed3d99405c12062d6322
Robert Hubbard <bobbyhubbard@gmail.com>
2015-03-25 18:36:35 +0000

    stashing

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -2,23 +2,34 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     breadCrumb: "Home",
-	breadCrumbIconClass: "ace-icon fa fa-home home-icon",
-	
-	watchSelectedValue: function(val){
-		console.log("here!!");
-	}.observes('selectedValue'),
-	
-	actions: {
-	    selectSuggestion: function(item){
-	        if(item && item.content){
-	            var className = item.content.toString();
-	            if(className.indexOf("organizations/profile") > 0){
-	                //transition
-	                console.log("transition to org " + item.get("id"));
-	            } else if(className.indexOf("contact") > 0){
-	                console.log("transition to contact " + item.get("id"));
-	            }
-	        }
-	    }
-  	}
+    breadCrumbIconClass: "ace-icon fa fa-home home-icon",
+
+    watchSelectedValue: function(item) {
+        console.log("here!!");
+        if (item) {
+            var className = item.toString();
+            if (className.indexOf("organizations/profile") > 0) {
+                //transition
+                console.log("transition to org " + item.get("id"));
+            }
+            else if (className.indexOf("contact") > 0) {
+                console.log("transition to contact " + item.get("id"));
+            }
+        }
+    }.observes('selectSuggestion'),
+
+    actions: {
+        selectItem: function(item) {
+            if (item) {
+                var className = item.toString();
+                if (className.indexOf("organizations/profile") > 0) {
+                    //transition
+                    console.log("transition to org " + item.get("id"));
+                }
+                else if (className.indexOf("contact") > 0) {
+                    console.log("transition to contact " + item.get("id"));
+                }
+            }
+        }
+    }
 });
