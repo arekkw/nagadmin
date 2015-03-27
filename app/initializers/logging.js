0ecbe93a3f2f2956a24e68f9f61c2430b7a12bc3
Michael Martz <blktiger@gmail.com>
2015-03-26 21:33:28 -0500

    Add the user id to raygun logs

--- app/initializers/logging.js
+++ app/initializers/logging.js
@@ -1,8 +1,10 @@
 /* global Raygun */
 import Ember from 'ember';
+import {fbEmailKeyScrubber} from 'nag-admin/helpers/fb-email-key-scrubber';
 
 export default {
 	name: 'logging',
+	after: 'authentication',
 
 	initialize: function(container, application) {
 		// Capture Exceptions and log them to Raygun
@@ -12,5 +14,13 @@ export default {
 		Ember.onerror = function(error) {
 			Raygun.send(error);
 		};
+
+		// Set the current user after login for Raygun errors
+		var firebase = container.lookup('firebase:connection');
+		firebase.onAuth(function(authentication) {
+			if (authentication && authentication.google) {
+				Raygun.setUser(authentication.google.email);
+			}
+		});
 	}
 };
