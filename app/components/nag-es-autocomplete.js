8bb3e87099e70a50f96b228348e3efa5aa085e83
Robert Hubbard <bobbyhubbard@gmail.com>
2015-03-28 00:28:14 +0000

    remove extra line break

--- app/components/nag-es-autocomplete.js
+++ app/components/nag-es-autocomplete.js
@@ -34,7 +34,6 @@ export default AutoComplete.extend({
                         "field": "suggest",
                         "size": 10,
                         "fuzzy": {
-                            
                             "fuzziness": 2
                         }
                     }
